db4198f255e6df3d8ab887e62923130d9a01e7a3
SebasShogun <shogun31sff@gmail.com>
2020-06-03 01:14:16 -0500

    Decoradores de identificación y Registration

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Auth redirects
+
+LOGIN_REDIRECT_URL = 'home'
