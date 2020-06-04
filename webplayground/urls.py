766109ee9369addfab9d94e00f0fa3563dec9868
SebasShogun <shogun31sff@gmail.com>
2020-06-04 03:02:04 -0500

    Paginación con listview y bootstrap

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
+from profiles.urls import profiles_patterns
 from django.conf import settings
 
 urlpatterns = [
@@ -25,6 +26,9 @@ urlpatterns = [
     #paths de auth
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
+    #paths de profiles
+    path('profiles/', include(profiles_patterns)),
+    #paths de Messenger
 ]
 
 if settings.DEBUG:
